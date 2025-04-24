ba24316ea708c3476005403372aa7b000303bf39
bolantej <114430388+bolantej@users.noreply.github.com>
2025-04-24 14:31:13 -0700

    Add files via upload

--- backend/src/tests/example_test.py
+++ backend/src/tests/example_test.py
@@ -1,2 +1,14 @@
+from api.users import login, signup, delete_user
+
 def test_example():
     assert 1
+
+#attempt to login to the venome admin account
+def test_login():
+    assert login({"admin@venome.cqls.oregonstate.edu", "password"}).token != ""
+
+#attempt to create and delete an account
+def test_account_creation():
+    assert signup({"test", "test@test.com", "test"}).error != ""
+    id = get_user_id("test")
+    assert delete_user(id) == 0
