50655795c97364dc23ea4e6f69437cf089279142
bolantej <114430388+bolantej@users.noreply.github.com>
2025-05-02 15:39:23 -0700

    Update example_test.py

--- backend/src/tests/example_test.py
+++ backend/src/tests/example_test.py
@@ -5,12 +5,13 @@ def test_example():
     assert 1
 
 #attempt to login to the venome admin account
-def test_login():
-    assert login({"admin@venome.cqls.oregonstate.edu", "password"}).token != ""
+#def test_login():
+#    assert login({"admin@venome.cqls.oregonstate.edu", "password"}).token != ""
 
 #attempt to create and delete an account
 def test_account_creation():
     body = SignupBody(username="test", email="test@test.com", password="test")
-    assert signup(body).error != ""
+    response = signup(body)
+    assert response.error == ""
     id = get_user_id("test")
     assert delete_user(id) == 0
