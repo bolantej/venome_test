b968d59e65f54cc0cf38734f4cd93ec4815febff
bolantej <114430388+bolantej@users.noreply.github.com>
2025-05-03 12:06:41 -0700

    Update example_test.py

--- backend/src/tests/example_test.py
+++ backend/src/tests/example_test.py
@@ -1,14 +1,8 @@
-from src.api.users import login, signup, delete_user, get_user_id
-from src.api_types import *
+from src.api.users import signup
+from src.api_types import SignupBody, SignupResponse
 
-def test_example():
-    assert 1
 
-#attempt to login to the venome admin account
-#def test_login():
-#    assert login({"admin@venome.cqls.oregonstate.edu", "password"}).token != ""
-
-#attempt to create and delete an account
+#attempt to create an account
 def test_account_creation():
     body = SignupBody(username="test", email="test@test.com", password="test")
     response = signup(body)
