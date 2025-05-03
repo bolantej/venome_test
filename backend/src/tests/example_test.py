4f7e3929bcc80a7e29a4a5b204b7b118b6549315
bolantej <114430388+bolantej@users.noreply.github.com>
2025-05-03 15:49:54 -0700

    Update example_test.py

--- backend/src/tests/example_test.py
+++ backend/src/tests/example_test.py
@@ -1,9 +1,26 @@
-from src.api.users import signup
-from src.api_types import SignupBody, SignupResponse
+from src.api.users import signup, delete_user, get_user_id, get_user, delete_user
+from src.api_types import SignupBody, SignupResponse, UserResponse, UserIDResponse
 
 
 #attempt to create an account
 def test_account_creation():
-    body = SignupBody(username="test", email="test@test.com", password="test")
+    body = SignupBody(username="test_user2", email="test@email.com", password="test")
     response: SignupResponse = signup(body)
     assert response.error == ""
+    id: UserIDResponse = get_user_id("test_user2")
+    assert id.id != -1
+
+def test_get_user():
+    response: UserResponse = get_user(1)
+    assert response.username == "test_user1"
+    assert response.email == "test@test.com"
+
+def test_account_deletion():
+    scope: Scope = {
+        "type": "http",
+        "headers": [(b"authorization", token.encode())],
+    }
+    delete_user(1, Request(scope))
+    response: UserResponse = get_user(1)
+    assert response.username == ""
+    assert response.email == ""
