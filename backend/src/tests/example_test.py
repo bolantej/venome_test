0b26af4a8070b068adbab1254b484826f91dfd8c
bolantej <114430388+bolantej@users.noreply.github.com>
2025-05-16 13:41:21 -0700

    Update example_test.py

--- backend/src/tests/example_test.py
+++ backend/src/tests/example_test.py
@@ -1,6 +1,17 @@
 from src.api.users import signup, delete_user, get_user_id, get_user, login #, delete_user
 from src.api_types import SignupBody, SignupResponse, UserResponse, UserIDResponse, LoginBody
 
+def create_dummy_request(auth_token: str) -> Request:
+    scope: Scope = {
+        "type": "http",
+        "headers": [
+            (b"authorization", f"Bearer {auth_token}".encode("utf-8")),
+        ],
+        "method": "GET",
+        "path": "/",
+    }
+    return Request(scope)
+
 
 #attempt to create an account
 def test_account_creation():
@@ -22,3 +33,13 @@ def test_get_user():
     response: UserResponse = get_user(1)
     assert response.username == "test_user1"
     assert response.email == "test@test.com"
+
+
+def test_account_deletion():
+    
+    token = generate_auth_token("test@test.com", admin=True)
+    req = create_dummy_request(token)
+    delete_user(1, req)
+    response: UserResponse = get_user(1)
+    assert response.username == ""
+    assert response.email == ""
